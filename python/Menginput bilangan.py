721bd2ac39ccb08f2761455431091cddeab644ac
dzunnurainarifmalika <113917569+dzunnurainarifmalika@users.noreply.github.com>
2023-03-02 09:12:21 +0700

    Add files via upload

---
+++ python/Menginput bilangan.py
@@ -0,0 +1,8 @@
+# Menginput bilangan
+bilangan = float(input("Masukkan bilangan: "))
+
+# Memeriksa apakah bilangan tersebut positif atau negatif
+if bilangan > 0:
+    print("Bilangan tersebut adalah bilangan positif.")
+else:
+    print("Bilangan tersebut adalah bilangan negatif.")
---
+++ python/luas.py
@@ -0,0 +1,5 @@
+# menghitung luas persegi panjang dengan python
+panjang =int (input("Masukan panjang : "))
+lebar =int (input("Masukan Lebar : "))
+luas =panjang*lebar
+print("Luas =",luas)
